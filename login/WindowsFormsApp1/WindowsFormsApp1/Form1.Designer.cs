bf01634c273280047bfe21adaf66d0752851ca87
谭尧丹 <38124648+Don2025@users.noreply.github.com>
2019-04-20 15:31:21 +0800

    简单的登录界面
    简单的登录界面

---
+++ login/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.安全登录 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(175, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 25);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(114, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "账号:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(175, 128);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(160, 25);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(114, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "密码:";
+            // 
+            // 安全登录
+            // 
+            this.安全登录.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.安全登录.Location = new System.Drawing.Point(130, 231);
+            this.安全登录.Name = "安全登录";
+            this.安全登录.Size = new System.Drawing.Size(216, 38);
+            this.安全登录.TabIndex = 4;
+            this.安全登录.Text = "安全登录";
+            this.安全登录.UseVisualStyleBackColor = true;
+            this.安全登录.Click += new System.EventHandler(this.ButtonLogin_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("微软雅黑", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.checkBox1.Location = new System.Drawing.Point(119, 177);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(87, 24);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "记住密码";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("微软雅黑", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.checkBox2.Location = new System.Drawing.Point(212, 177);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(87, 24);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "自动登录";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(482, 303);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.安全登录);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "WeChat";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button 安全登录;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ login/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonLogin_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text.Trim();  //获取用户输入的账号
+            string password = textBox2.Text.Trim();  //获取用户输入的密码
+            /*
+             * 操作sql Server数据库的五大对象：
+               SqlConnection：创建数据库连接对象
+               SqlCommand：执行SQL语句对象
+               SqlDataReader：创建一个查询一条或多条数据的对象
+               SqlDataAdapter：创建一个用于检索和保存数据的对象
+               DataSet：创建一个本地数据存储对象
+             */
+            //与sql sever建立连接
+            SqlConnection connection = new SqlConnection("server=.;User ID=sa;Password=sql123");
+            connection.Open();   //打开连接
+
+            /*
+             * ExecuteReader（）方法可以执行SELECT语句，
+             * 我们可以用一个叫做SqlDataReader的对象来接收查询的结果集
+             * SELECT语句执行后，在data里就得到了所有的usertable 表里的信息了
+             */
+            //用SqlCommand来执行SQL语句:SELECT userid,password FROM usertable WHERE userid = (用户输入的username) AND password = (用户输入的password);
+            SqlCommand SqlCommand = new SqlCommand("SELECT userid,password FROM usertable WHERE userid ='"+username+"' AND password ='"+password+"'", connection);
+            SqlDataReader data = SqlCommand.ExecuteReader();
+            data.Read();
+            if (String.IsNullOrEmpty(username) || String.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("用户名或密码不能为空！", "警告", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (data.HasRows)  //HasRows用来判断查询结果中是否有数据
+            {
+                MessageBox.Show("登录成功！","通知",MessageBoxButtons.OK,MessageBoxIcon.Asterisk);             //登录成功
+            }
+            else
+            {
+                MessageBox.Show("登录失败！", "警告", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            data.Close();   //关闭SqlDataReader对象
+            connection.Close();  //关闭连接
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)   //用户输入完毕时敲回车键可以进行登录
+            {
+                ButtonLogin_Click(sender,e);
+            }
+        }
+    }
+}
